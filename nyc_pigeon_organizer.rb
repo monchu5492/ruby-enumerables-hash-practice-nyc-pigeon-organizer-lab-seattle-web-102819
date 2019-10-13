72cd003a7954fe4138e374b3587c79d6da1f0cf3
Tanner Patterson <tpatterson5492@gmail.com>
2019-10-13 02:14:21 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,69 @@
+require "pry"
+
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+new_hash = {}
+
+data.each do |key, value| 
+  value.each do |attribs, names|
+    names.each do |name|
+  
+      if !new_hash.has_key?(name)
+        new_hash[name] = {}
+      end
+      if !new_hash[name][key]
+        new_hash[name][key] = []
+      end
+    end     
+   end     
+  end 
+ new_hash
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# require "pry"
+
+
+# def nyc_pigeon_organizer(data)
+# org_p_data = {}
+  
+#   data.find do |attribs, values|
+#     values.each do |key, value|
+#   if org_p_data.has_key?(values)
+#     org_p_data = data[attribs][values].each_value {
+#       |nams| nams }
+#   org_p_data[values].each_value 
+# else final_cart[element_name] = {
+       
+#     binding.pry
+#   end 
+#   end
+# end
+# end
